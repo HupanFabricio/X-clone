e5901b92720d89938ff9de7bbfb81329f122d1ed
Fabricio Hupan <fabrihupan@gmail.com>
2024-10-30 00:32:20 -0300

    tweets

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,15 +1,28 @@
+import { createClient } from "@/utils/supabase/server";
 import { AuthButton } from "../components/auth-button";
-import { handleSignIn, login, signup } from "./actions";
+// import { handleSignIn, login, signup } from "./actions";
+import { handleSignIn } from "./actions";
+import { redirect } from "next/navigation";
 
-export default function LoginPage() {
+export default async function LoginPage() {
+	const supabase = await createClient();
+
+	const {
+		data: { user },
+		error,
+	} = await supabase.auth.getUser();
+
+	if (user) {
+		redirect("/");
+	}
 	return (
-		<form className="flex flex-col gap-4 max-w-2xl m-auto justify-center min-h-screen bg-slate-300/10">
-			<label htmlFor="email">Email:</label>
+		<form className="flex flex-col items-center gap-4 max-w-[400px] m-auto justify-center min-h-screen">
+			{/* <label htmlFor="email">Email:</label>
 			<input id="email" name="email" type="email" />
 			<label htmlFor="password">Password:</label>
 			<input id="password" name="password" type="password" />
 			<button formAction={login}>Log in</button>
-			<button formAction={signup}>Sign up</button>
+			<button formAction={signup}>Sign up</button> */}
 			<AuthButton handleFunction={handleSignIn} />
 		</form>
 	);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,34 @@
+import { createClient } from "@/utils/supabase/server";
+
 export default async function Home() {
+	const supabase = await createClient();
+
+	const {
+		data: { user },
+	} = await supabase.auth.getUser();
+
+	const { data: post } = await supabase.from("posts").select();
+
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			Hola X 👋
+			{user ? (
+				<div className="gap-y-4 justify-center flex flex-col items-center">
+					<h1 className="text-2xl font-bold">
+						<span className="opacity-85">Bienvenido </span>
+						<span className="opacity-100">
+							{user.user_metadata.user_name}
+						</span>
+						<span className="opacity-85"> estas logueado 👋</span>
+					</h1>
+					<button className="text-white max-w-[300px] justify-center bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 me-2 mb-2">
+						Cerrar session
+					</button>
+				</div>
+			) : (
+				"Hola X NO estas logueado😌"
+			)}
+
+			<pre>{JSON.stringify(post, null, 2)}</pre>
 		</main>
 	);
 }
